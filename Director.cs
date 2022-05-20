6df8d4c1f6d8fcbffcadb5d8ca3bf9b920a2174c
Caleb Wolfert <calebwolfert@gmail.com>
2022-05-20 17:07:55 -0600

    director

--- Director.cs
+++ Director.cs
@@ -9,9 +9,6 @@ namespace Jumper
         {
             //Generates the users random word
             string randomWord = word.WordGen();
-
-            int num = 0;
-            parachute.PrintParachute(num);
         }
     }
 }
